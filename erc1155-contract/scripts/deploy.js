f0d0dbbbdf12ccb3ae7001c0e141efb86f3eccf8
Christopher Cialone <ccialone0929@gmail.com>
2023-01-02 18:00:58 -0500

    First commit

---
+++ erc1155-contract/scripts/deploy.js
@@ -0,0 +1,26 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+
+  const factory = await hre.ethers.getContractFactory("AwesomeGame");
+  const [owner] = await hre.ethers.getSigners();
+  const contract = await factory.deploy();
+
+  await contract.deployed();
+  console.log("Contract deployed to: ", contract.address);
+  console.log("Contract deployed by: ", owner.address, "\n");
+  console.log("Tokens have been minted successfully!");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
